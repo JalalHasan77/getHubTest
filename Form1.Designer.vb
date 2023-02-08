df9e2f85cc1cb03a585965c0b76ae949fde5cf88
JalalHasan77 <123944947+JalalHasan77@users.noreply.github.com>
2023-02-08 12:08:13 +0300

    return to previous version

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -68,7 +68,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(747, 450)
+        Me.ClientSize = New System.Drawing.Size(611, 450)
         Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.PictureBox1)
