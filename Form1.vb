0219c1f33fce658cffd8576c05d303db975cd28f
JalalHasan77 <123944947+JalalHasan77@users.noreply.github.com>
2023-02-08 11:04:55 +0300

    Opef File Dialoge

--- Form1.vb
+++ Form1.vb
@@ -1,6 +1,12 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        PictureBox1.Image = Image.FromFile("\\HQPROFILES\HQUP$\2271\Desktop\20170919133247-GettyImages-607980182.jpeg")
-        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        Dim [OF] As New OpenFileDialog
+        If [OF].ShowDialog = DialogResult.OK Then
+
+
+
+            PictureBox1.Image = Image.FromFile([OF].FileName)
+            PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        End If
     End Sub
 End Class
