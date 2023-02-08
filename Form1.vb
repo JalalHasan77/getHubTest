07361f6e23c91976d8716474194a79adbde7eb0b
JalalHasan77 <123944947+JalalHasan77@users.noreply.github.com>
2023-02-08 11:00:22 +0300

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PictureBox1.Image = Image.FromFile("\\HQPROFILES\HQUP$\2271\Desktop\20170919133247-GettyImages-607980182.jpeg")
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+    End Sub
+End Class
