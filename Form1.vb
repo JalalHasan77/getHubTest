55983bbed52d346bb25d395259bc4e79413cf0f1
JalalHasan77 <123944947+JalalHasan77@users.noreply.github.com>
2023-02-08 12:13:04 +0300

    i added a commit

--- Form1.vb
+++ Form1.vb
@@ -5,7 +5,7 @@
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-
+        'anything
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
